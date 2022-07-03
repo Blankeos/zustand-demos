fbcc5bfc8fc295601208874b32e23c9f09ec1780
Blankeos <carloantonioct@gmail.com>
2022-07-04 00:35:34 +0800

    Cleanest middleware and type compatibility

--- stores/counterStore.ts
+++ stores/counterStore.ts
@@ -1,20 +1,19 @@
 import create, { GetState, SetState, StateCreator, StoreApi } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-interface State {
+interface IState {
   // States
   count: number;
-  // Actions
-  increase: (by: number) => void;
-  decrease: (by: number) => void;
+  // No Need to include actions
 }
-
-const store: StateCreator<State, [["zustand/devtools", never]]> = (set) => ({
+const store = (set: SetState<IState>) => ({
   // States
   count: 1,
   // Actions
-  increase: (by) => set((state) => ({ count: state.count + by })),
-  decrease: (by) => set((state) => ({ count: state.count - by })),
+  increase: (by: number) =>
+    set((state: IState) => ({ count: state.count + by })),
+  decrease: (by: number) =>
+    set((state: IState) => ({ count: state.count - by })),
 });
 
-export const useCounterStore = create(devtools(store));
+export const useCounterStore = create(devtools(persist(store)));
